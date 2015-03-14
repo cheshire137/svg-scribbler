652fe39c6b9346068e6742de1787b706a1099b48
Sarah Vessels <sarah.vessels@cirrusmio.com>
2015-03-13 20:12:43 -0400

    Add initial ReactJS app with Gulp

---
+++ app/scripts/app.js
@@ -0,0 +1,53 @@
+/** @jsx React.DOM */
+
+var React = window.React = require('react'),
+    Timer = require('./ui/Timer'),
+    mountNode = document.getElementById('app');
+
+var TodoList = React.createClass({
+  render: function() {
+    var createItem = function(itemText) {
+      return <li>{itemText}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+});
+
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {items: [], text: '', shape: 'circle'};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  onChooseShape: function(e) {
+    this.setState({shape: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
+  render: function() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <select class="form-control" onChange={this.onChooseShape}>
+            <option value="circle">circle</option>
+            <option value="rect">rectangle</option>
+            <option value="ellipse">ellipse</option>
+          </select>
+          <p>currently: {this.state.shape}</p>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+        <Timer />
+      </div>
+    );
+  }
+});
+
+React.render(<TodoApp />, mountNode);
