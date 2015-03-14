970d30086117b0d0465ea38ccbf615b6f956fa4c
Sarah Vessels <sarah.vessels@cirrusmio.com>
2015-03-14 16:30:12 -0400

    Draw multiple lines in different colors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,59 +6,62 @@ var React = window.React = require('react'),
 var SvgScribblerApp = React.createClass({
   getInitialState: function() {
     return {
-      points: [],
-      stroke: 'rgb(255,0,0)',
-      fill: 'rgb(255,255,255)',
-      strokeWidth: 3,
+      lines: [{
+        points: [],
+        stroke: '#ff0000',
+        fill: '#ffffff',
+        strokeWidth: 3
+      }],
       isDrawing: false
     };
   },
   componentDidMount: function() {
     var self = this;
     $('.color-picker').spectrum({
-      color: this.state.stroke,
+      color: this.state.lines[0].stroke,
       change: function(color) {
-        self.setState({stroke: color.toRgbString()});
+        var line = self.getCurrentLine();
+        line.stroke = color.toRgbString();
+        self.updateCurrentLine(line);
       }
     });
   },
-  onAddPoint: function(e) {
-    var canvas = $(e.target);
-    var offset = canvas.offset();
-    var left = offset.left;
-    var top = offset.top;
-    var x = e.pageX - left;
-    var y = e.pageY - top;
-    var clickedExistingPoint = false;
-    var points = this.state.points;
-    for (var i=0; i<this.state.points.length; i++) {
-      var point = this.state.points[i];
-      if (y > point.top && y < point.top + point.height &&
-          x > point.left && x < point.left + point.width) {
-        alert('clicked a point', point);
-        clickedExistingPoint = true;
-      }
-    }
-    if (!clickedExistingPoint) {
-      this.addPoint({x: x, y: y});
-    }
-  },
-  doesPointExist: function(coords) {
-    for (var i=0; i<this.state.points.length; i++) {
-      var point = this.state.points[i];
+  doesPointExist: function(line, coords) {
+    for (var i=0; i<line.length; i++) {
+      var point = line[i];
       if (point.left === coords.x && point.top === coords.y) {
         return true;
       }
     }
     return false;
   },
-  addPoint: function(coords) {
-    var newPoints = this.state.points;
-    if (!this.doesPointExist(coords)) {
-      var newPoint = {left: coords.x, top: coords.y, width: 10, height: 10};
-      newPoints = newPoints.concat([newPoint]);
+  getCurrentLine: function() {
+    var line = this.state.lines.slice(this.state.lines.length - 1)[0];
+    var newLine = {};
+    var props = Object.keys(line);
+    for (var i=0; i<props.length; i++) {
+      var prop = props[i];
+      var originalValue = line[prop];
+      if (Array.isArray(originalValue)) {
+        newLine[prop] = originalValue.slice(0, originalValue.length);
+      } else {
+        newLine[prop] = originalValue;
+      }
     }
-    this.setState({points: newPoints});
+    return newLine;
+  },
+  updateCurrentLine: function(line) {
+    var newLines = this.state.lines.slice(0, this.state.lines.length - 1);
+    newLines.push(line);
+    this.setState({lines: newLines});
+  },
+  addPoint: function(coords) {
+    var line = this.getCurrentLine();
+    if (!this.doesPointExist(line, coords)) {
+      var newPoint = {left: coords.x, top: coords.y, width: 10, height: 10};
+      line.points.push(newPoint);
+      this.updateCurrentLine(line);
+    }
   },
   getCanvasCoords: function(e) {
     var canvas = $(e.target);
@@ -68,6 +71,18 @@ var SvgScribblerApp = React.createClass({
     var x = e.pageX - left;
     var y = e.pageY - top;
     return {x: x, y: y};
+  },
+  addNewLineIfNecessary: function() {
+    var lastLine = this.getCurrentLine();
+    if (lastLine.points.length > 0) {
+      var newLine = {
+        points: [],
+        stroke: '#ff0000',
+        fill: '#ffffff',
+        strokeWidth: 3
+      };
+      this.setState({lines: this.state.lines.concat(newLine)});
+    }
   },
   onMouseDown: function(e) {
     this.setState({isDrawing: true});
@@ -82,35 +97,49 @@ var SvgScribblerApp = React.createClass({
     this.addPoint(coords);
   },
   onMouseUp: function(e) {
-    this.setState({isDrawing: false});
     var coords = this.getCanvasCoords(e);
     this.addPoint(coords);
+    this.setState({isDrawing: false});
+    this.addNewLineIfNecessary();
   },
   onMouseLeave: function(e) {
     this.setState({isDrawing: false});
+    this.addNewLineIfNecessary();
   },
-  getPointsList: function() {
+  getPointsList: function(line) {
     var pairs = [];
-    for (var i=0; i<this.state.points.length; i++) {
-      var point = this.state.points[i];
+    for (var i=0; i<line.points.length; i++) {
+      var point = line.points[i];
       pairs.push(point.left + ',' + point.top);
     }
     return pairs.join(' ');
   },
-  shapeStyle: function() {
+  getPolylineStyle: function(line) {
     return {
-      stroke: this.state.stroke,
-      fill: this.state.fill,
-      strokeWidth: 3
+      stroke: line.stroke,
+      fill: line.fill,
+      strokeWidth: line.strokeWidth
     };
   },
-  chooseColorStyle: function() {
-    return {
-      backgroundColor: this.state.stroke,
-      color: this.state.fill
-    };
+  getPolylineSource: function() {
+    var polylines = [];
+    for (var i=0; i<this.state.lines.length; i++) {
+      var line = this.state.lines[i];
+      if (line.points.length < 1) {
+        continue;
+      }
+      var svgPoints = [];
+      for (var j=0; j<line.points.length; j++) {
+        var point = line.points[j];
+        svgPoints.push(point.left + ',' + point.top);
+      }
+      svgPoints = svgPoints.join(' ');
+      polylines.push('  <polyline points="' + svgPoints + '" />');
+    }
+    return polylines.join("\n") + "\n";
   },
   render: function() {
+    var self = this;
     return (
       <div>
         <div className="row">
@@ -120,14 +149,18 @@ var SvgScribblerApp = React.createClass({
             </p>
             <div className="svg-container clearfix">
               <svg className="svg-result">
-                <polyline points={this.getPointsList()} style={this.shapeStyle()} />
+                {
+                  this.state.lines.map(function(line) {
+                    return <polyline points={self.getPointsList(line)} style={self.getPolylineStyle(line)} />
+                  })
+                }
               </svg>
               <canvas className="svg-canvas" onMouseDown={this.onMouseDown} onMouseMove={this.onMouseMove} onMouseUp={this.onMouseUp} onMouseLeave={this.onMouseLeave}></canvas>
             </div>
           </div>
           <div className="col-md-6">
             <pre>&lt;svg&gt;<br />
-  &lt;polyline points="{this.getPointsList()}" style="stroke: {this.state.stroke}; fill: {this.state.fill}; stroke-width: {this.state.strokeWidth};" /&gt;<br />
+  {this.getPolylineSource()}
 &lt;/svg&gt;</pre>
           </div>
         </div>
